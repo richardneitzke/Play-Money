047134046dc783ca8b6d1bcf76bc4cf36e249c63
Richard Neitzke <richardneitzke@icloud.com>
2015-10-12 18:41:03 -0400

    Initial Commit

---
+++ Play Money/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Play Money
+//
+//  Created by Richard Neitzke on 10/12/15.
+//  Copyright © 2015 Richard Neitzke. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
